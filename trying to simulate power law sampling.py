b5f626e6616915a5454045ab44bf28fa0de05a62
Minhal <minhalhasham@gmail.com>
2022-03-14 23:11:20 -0400

    Attempt at sampling power-law to generate blinking trace

---
+++ trying to simulate power law sampling.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def power_law(t,a):
+    return t**-a
+
+
+time=np.arange(10**-4,1,10**-4)
+
+emp=[]
+for i in range(1000): 
+    emp.append(np.random.choice(power_law(time,1.5)/max(power_law(time,1.5))))
+
+
+
+
+
+
+plt.plot(emp)
