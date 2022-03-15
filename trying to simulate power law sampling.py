fbef86b48d54111e795f1401cbbe63845c2834ec
Minhal <minhalhasham@gmail.com>
2022-03-14 23:43:30 -0400

    idk fam

--- trying to simulate power law sampling.py
+++ trying to simulate power law sampling.py
@@ -1,19 +1,27 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+
 def power_law(t,a):
     return t**-a
 
 
 time=np.arange(10**-4,1,10**-4)
 
+y=power_law(time,1.5)
+
+y_norm=y/sum(y)
+
+
+print(sum(y_norm))
+
 emp=[]
-for i in range(1000): 
-    emp.append(np.random.choice(power_law(time,1.5))
+new=[]
+for i in range(len(time)): 
+    emp.append(np.random.choice(power_law(time[i],1.5)))
 
 
-
-
-
-
-plt.plot(emp)
+emp2=[]
+for i in time: 
+    emp2.append(power_law(i,1.5))
+plt.loglog(time,emp2)
